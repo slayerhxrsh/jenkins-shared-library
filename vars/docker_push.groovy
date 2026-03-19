5e827b7c0e93aef68f5f59e58ea9bc0affd8ee42
Harsh Srivastava <harsh.sep14@gmail.com>
2026-03-19 16:04:28 +0530

    Improve readability of docker login command
    Refactor docker login command for better readability.

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,7 +1,9 @@
 def call(String Project, String ImageTag, String dockerhubuser){
   withCredentials([usernamePassword(credentialsId: 'dockerHubCred', passwordVariable: 'dockerHubPass', usernameVariable: 'dockerHubUser')]) {
       
-      sh "echo "${dockerHubPass}" | docker login -u "${dockerHubUser}" --password-stdin "
+      sh """
+        echo "${dockerHubPass}" | docker login -u "${dockerHubUser}" --password-stdin 
+      """
   }
   
   sh "docker push ${dockerhubuser}/${Project}:${ImageTag}"
